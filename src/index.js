32c26c5e9ae9ed1b0769b4de2ddf610ccc58575c
ketan0045 <ketankalsariya0902@gmail.com>
2024-07-10 12:47:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Today's Tasks:
+// Project: Training
+// - Create a demo for practices of Redux Toolkit for state management in React applications using api and api intregation.
+// - Implement an external API to  dynamic state updates and make simple UI.
